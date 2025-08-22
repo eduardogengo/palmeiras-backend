44a957bf4c234721f79e36a76206929239c67c8d
Eduardo Gengo <eduardogengo@yahoo.com.br>
2025-08-22 10:42:42 -0300

    cors habilitado

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const pool = require('./db');
 const app = express();
 const PORT = 3000;
 
+// cors habilitado pra todos
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json()); // para ler JSON do body
 
 // rota raiz
