ecc011030665cdea966149c9e32668237d673068
Eduardo Gengo <eduardogengo@yahoo.com.br>
2025-08-27 09:08:07 -0300

    comentário na linha 16

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
 
 app.use(express.json()); // para ler JSON do body
 // teste do primeiro commit
+//  alteração do commit do EDU
 // rota raiz
 app.get("/", (req, res) => {
   res.status(200).send("Servidor rodando!");
