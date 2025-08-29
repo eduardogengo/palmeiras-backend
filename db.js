f25ff02fd1fcf0a73de071105157b41216069aa9
Eduardo Gengo <eduardogengo@yahoo.com.br>
2025-08-29 11:43:35 -0300

    adaptacao ps postgres

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 const postgres = require('postgres');
 
 const senha = 'edumarciopalmeiras';
-// const connectionString = 'postgresql://postgres:edumarciopalmeiras@db.viujexlrejrubtphywbi.supabase.co:5432/postgres';
 const connectionString = `postgresql://postgres.viujexlrejrubtphywbi:${senha}@aws-1-sa-east-1.pooler.supabase.com:5432/postgres`;
-// const connectionString =  `postgres.viujexlrejrubtphywbi:${senha}@aws-1-sa-east-1.pooler.supabase.com:6543/postgres`
-console.log('connectionString', connectionString);
 const sql = postgres(connectionString);
 
 module.exports = sql;
