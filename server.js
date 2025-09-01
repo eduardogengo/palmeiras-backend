798494c5dcec329945a79e60e47044d266f8c98c
Eduardo Gengo <eduardogengo@yahoo.com.br>
2025-09-01 14:43:31 -0300

    alteracao na query do listar jogador - traz o campo descricao da posicao

--- server.js
+++ server.js
@@ -1,92 +1,100 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const sql = require("./db");
+const sql = require('./db');
 const PORT = 3000;
-const { parseISO, addDays, isSaturday, isSunday } = require("date-fns");
+const { parseISO, addDays, isSaturday, isSunday } = require('date-fns');
 
 // cors habilitado pra todos
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 app.use(express.json());
 
 // rota raiz
-app.get("/", (req, res) => {
-  res.status(200).send("Servidor rodando!");
+app.get('/', (req, res) => {
+  res.status(200).send('Servidor rodando!');
 });
 
 // listar todos
-app.get("/jogadores", async (req, res) => {
+app.get('/jogadores', async (req, res) => {
   try {
-    const rows = await sql`SELECT * FROM jogador`;
+    const rows = await sql`
+    SELECT 
+      jogador.id, 
+      jogador.nome, 
+      posicao.descricao
+    FROM jogador
+      JOIN posicao 
+        ON jogador.id_posicao = posicao.id;
+`;
     res.json(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Erro ao buscar jogadores" });
+    res.status(500).json({ error: 'Erro ao buscar jogadores' });
   }
 });
 // listar todo
-app.get("/posicao", async (req, res) => {
+app.get('/posicao', async (req, res) => {
   try {
     const rows = await sql`SELECT * FROM posicao`;
     res.json(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Erro ao buscar jogadores" });
+    res.status(500).json({ error: 'Erro ao buscar jogadores' });
   }
 });
 
 // detalhar 1
-app.get("/jogadores/:id", async (req, res) => {
+app.get('/jogadores/:id', async (req, res) => {
   const id = parseInt(req.params.id);
-  if (isNaN(id)) return res.status(400).json({ erro: "ID inválido" });
+  if (isNaN(id)) return res.status(400).json({ erro: 'ID inválido' });
 
   try {
     const [row] = await sql`SELECT * FROM jogador WHERE id = ${id}`;
-    if (!row) return res.status(404).json({ erro: "Jogador não encontrado" });
+    if (!row) return res.status(404).json({ erro: 'Jogador não encontrado' });
     res.json(row);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ erro: "Erro ao buscar jogador" });
+    res.status(500).json({ erro: 'Erro ao buscar jogador' });
   }
 });
 
 // criar
-app.post("/jogadores", async (req, res) => {
+app.post('/jogadores', async (req, res) => {
   const { nome, idPosicao } = req.body;
   if (!nome || !idPosicao)
-    return res.status(400).json({ erro: "Nome e idPosição são obrigatórios" });
+    return res.status(400).json({ erro: 'Nome e idPosição são obrigatórios' });
 
   try {
     await sql`INSERT INTO jogador (nome, id_posicao) VALUES (${nome}, ${idPosicao})`;
-    res.status(201).json({ mensagem: "Jogador criado com sucesso" });
+    res.status(201).json({ mensagem: 'Jogador criado com sucesso' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ erro: "Erro ao criar jogador" });
+    res.status(500).json({ erro: 'Erro ao criar jogador' });
   }
 });
 
 // excluir
-app.delete("/jogadores/:id", async (req, res) => {
+app.delete('/jogadores/:id', async (req, res) => {
   const id = parseInt(req.params.id);
-  if (isNaN(id)) return res.status(400).json({ erro: "ID inválido" });
+  if (isNaN(id)) return res.status(400).json({ erro: 'ID inválido' });
 
   try {
     const result = await sql`DELETE FROM jogador WHERE id = ${id} RETURNING *`;
     if (result.length === 0)
-      return res.status(404).json({ erro: "Jogador não encontrado" });
-    res.json({ mensagem: "Jogador excluído com sucesso" });
+      return res.status(404).json({ erro: 'Jogador não encontrado' });
+    res.json({ mensagem: 'Jogador excluído com sucesso' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ erro: "Erro ao excluir jogador" });
+    res.status(500).json({ erro: 'Erro ao excluir jogador' });
   }
 });
 
 // Endpoint para adicionar dias úteis a uma data
-app.post("/adicionar-dias-uteis", (req, res) => {
+app.post('/adicionar-dias-uteis', (req, res) => {
   const { dataInicio, diasParaAdicionar } = req.body;
 
   if (!dataInicio || diasParaAdicionar === undefined)
@@ -102,7 +110,7 @@ app.post("/adicionar-dias-uteis", (req, res) => {
     if (!isSaturday(dataAtual) && !isSunday(dataAtual)) diasAdicionados++;
   }
 
-  const dataFinal = dataAtual.toISOString().split("T")[0];
+  const dataFinal = dataAtual.toISOString().split('T')[0];
   res.json({
     dataInicio,
     diasAdicionados: diasParaAdicionar,
@@ -111,33 +119,33 @@ app.post("/adicionar-dias-uteis", (req, res) => {
 });
 
 // Lição de casa
-app.get("/dia-da-semana/:data", (req, res) => {
+app.get('/dia-da-semana/:data', (req, res) => {
   const { data } = req.params;
   try {
     const diaDaSemana = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
     ][new Date(data).getUTCDay()];
     if (!diaDaSemana)
       return res
         .status(400)
-        .json({ error: "Data inválida. Use formato YYYY-MM-DD" });
+        .json({ error: 'Data inválida. Use formato YYYY-MM-DD' });
     res.json({ diaDaSemana });
   } catch (err) {
-    res.status(400).json({ error: "Erro ao processar a data." });
+    res.status(400).json({ error: 'Erro ao processar a data.' });
   }
 });
 
 // teste de conexão com o banco
-app.get("/teste-conexao-db", async (req, res) => {
+app.get('/teste-conexao-db', async (req, res) => {
   try {
     const [row] = await sql`SELECT NOW()`;
-    res.json({ message: "Conexão bem-sucedida!", time: row.now });
+    res.json({ message: 'Conexão bem-sucedida!', time: row.now });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: err.message });
