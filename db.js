4e3ccc51fa187753a5ad0b0fd4d77d41b9104167
Eduardo Gengo <eduardogengo@yahoo.com.br>
2025-09-01 10:59:00 -0300

    codigo utilizando variavel de ambiente para conexao com db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const postgres = require('postgres');
 
-const senha = 'edumarciopalmeiras';
-const connectionString = `postgresql://postgres.viujexlrejrubtphywbi:${senha}@aws-1-sa-east-1.pooler.supabase.com:5432/postgres`;
+const connectionString = process.env.DATABASE_URL;
 const sql = postgres(connectionString);
 
 module.exports = sql;
